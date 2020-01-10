3dc716552dc596b55c26b88ffb927337d3e9ac4b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 01:38:20 +0000

    added list_by_name method and fixed menu display

--- lib/pokemon_info/cli.rb
+++ lib/pokemon_info/cli.rb
@@ -2,19 +2,37 @@ class PokemonInfo::CLI
 
   def call
     input = ""
-    unless input.downcase == "exit"
+    until input.downcase == "exit"
       self.display_menu
       input = gets.chomp
+      if input == "1"
+        self.list_by_name
+      elsif input == "2"
+        self.list_by_number
+      elsif input == "3"
+        self.search_name
+      elsif input == "4"
+        self.search_number
+      elsif input.downcase == "exit"
+        puts "Thanks for using Pokemon Info, Cya later!"
+      else
+        puts "Sorry, I don't know what you mean by #{input}. Please try again"
+      end
     end
   end
 
   def display_menu
-    puts "Please choose one of the following options"
+    puts "Please choose one of the following options or type 'exit' to quit."
+    puts "---------------------------------------------------------------------"
     puts "1. List Pokemon by name"
     puts "2. List Pokemon by number"
     puts "3. Search by name"
     puts "4. Search by number"
-    # puts "Here is some info about Pokemon..."
-    # puts "Well there will be eventually..."
+    puts "---------------------------------------------------------------------"
+  end
+
+  def list_by_name
+    puts "Here's a list of all the Pokemon in Alphabetical order!"
+    puts "---------------------------------------------------------------------"
   end
 end
