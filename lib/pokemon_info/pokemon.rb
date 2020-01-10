3e274971abdbe7b9d6e7b6aa81a53fb5304bd709
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 08:23:32 +0000

    changing add_details method, currently broken

--- lib/pokemon_info/pokemon.rb
+++ lib/pokemon_info/pokemon.rb
@@ -25,7 +25,6 @@ class PokemonInfo::Pokemon
   end
 
   def self.create_from_pokemondb
-    # use Pokemon.scrape_from_official_site to make pokemon
     PokemonInfo::Pokemon.scrape_from_pokemondb
     PokemonInfo::Pokemon.add_details_to_all_pokemon
   end
@@ -36,10 +35,10 @@ class PokemonInfo::Pokemon
     pokemon = page.css("div.infocard-list.infocard-list-pkmn-lg div.infocard")
     pokemon.each do |mon|
       info = mon.css("span.infocard-lg-data.text-muted")
-      name = info.css("a")[0].text
+      name = info.css("a")[0].text #holds pokemon name
       types = []
       if info.css("a").size ==  3
-        types = [info.css("a")[1].text, info.css("a")[2].text]
+        types = [info.css("a")[1].text, info.css("a")[2].text] # [1] and [2] hold the types
       else
         types = [info.css("a")[1].text]
       end
@@ -52,7 +51,23 @@ class PokemonInfo::Pokemon
     @@all.each do |pokemon|
       if pokemon.number < 810
         specs_page = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/#{pokemon.name.downcase}"))
+        # adding abilities to pokemon
+        # abilities = specs_page.css("div.pokemon-ability-info.color-bg.color-lightblue.match.active div.pokemon-ability-info-detail.match")
+        # abilities_and_info = []
+        # abilities.each do |ability|
+        #   abilities_and_info << ability.css("h3").text #the name of the ability
+        #   abilities_and_info << ability.css("p").text #description of ability
+        # end
+        # pokemon.abilities = abilities_and_info
+        # adding weaknesses to pokemon
+        # weaknesses_raw = specs_page.css("div.pokedex-pokemon-attributes.active div.dtm-weaknesses ul li")
+        # weaknesses = []
+        # weaknesses_raw.each do |weakness|
+        #   weaknesses << weakness.css("a span").text
+        # end
+        # pokemon.weaknesses = weaknesses
       end
     end
+    binding.pry
   end
 end
