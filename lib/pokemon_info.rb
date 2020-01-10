4f4f20b43139937650e5e50ff4d3751c0f616e8a
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 05:38:54 +0000

    added nokogiri for scraping

--- lib/pokemon_info.rb
+++ lib/pokemon_info.rb
@@ -1,2 +1,6 @@
+require "open-uri"
+require "nokogiri"
+require "pry" #take out after developement
 require_relative "./pokemon_info/version"
+require_relative "./pokemon_info/pokemon"
 require_relative "./pokemon_info/cli"
--- lib/pokemon_info/pokemon.rb
+++ lib/pokemon_info/pokemon.rb
@@ -1,13 +1,27 @@
 class PokemonInfo::Pokemon
-  attr_accessor :name, :number, :types, :weaknesses
+  attr_accessor :name, :number, :types, :info_link, :weaknesses, :abilities
   @@all = []
 
-  def initialize(name, number, types, weaknesses)
+  def initialize(name, number, types, info_link)
     @name = name
     @number = number
     @types = types
-    @weaknesses = weaknesses
+    @info_link = info_link
 
     @@all << self
+  end
+
+  def self.create_from_official_site
+    # use Pokemon.scrape_from_official_site to make pokemon
+  end
+
+  def self.scrape_from_official_site
+    # uses nokogiri and open-uri to pull
+    page = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/"))
+  end
+
+  def self.scrape_info_link
+    # uses info link and returns extra info to make Pokemon
+    specs_page = Nokogiri::HTML(open(@info_link))
   end
 end
