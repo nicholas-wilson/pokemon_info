f6f6a443df7874e44ac5cf7f3a2796075cce9d54
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 05:01:20 +0000

    added initialize method for Pokemon

--- lib/pokemon_info/pokemon.rb
+++ lib/pokemon_info/pokemon.rb
@@ -1,3 +1,13 @@
 class PokemonInfo::Pokemon
+  attr_accessor :name, :number, :types, :weaknesses
+  @@all = []
 
+  def initialize(name, number, types, weaknesses)
+    @name = name
+    @number = number
+    @types = types
+    @weaknesses = weaknesses
+
+    @@all << self
+  end
 end
