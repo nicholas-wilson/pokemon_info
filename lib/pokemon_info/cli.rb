ad593c77396ce672307eb3aa8a200751e52a3c66
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 01:18:53 +0000

    added display_menu instance method for PokemonInfo::CLI

--- lib/pokemon_info/cli.rb
+++ lib/pokemon_info/cli.rb
@@ -1,7 +1,20 @@
 class PokemonInfo::CLI
 
   def call
-    puts "Here is some info about Pokemon..."
-    puts "Well there will be eventually..."
+    input = ""
+    unless input.downcase == "exit"
+      self.display_menu
+      input = gets.chomp
+    end
+  end
+
+  def display_menu
+    puts "Please choose one of the following options"
+    puts "1. List Pokemon by name"
+    puts "2. List Pokemon by number"
+    puts "3. Search by name"
+    puts "4. Search by number"
+    # puts "Here is some info about Pokemon..."
+    # puts "Well there will be eventually..."
   end
 end
