4ef0a592b79e761288d0cf10b119acc00084348d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-10 01:59:24 +0000

    removed list_by_name method for CLI

--- lib/pokemon_info/cli.rb
+++ lib/pokemon_info/cli.rb
@@ -6,12 +6,10 @@ class PokemonInfo::CLI
       self.display_menu
       input = gets.chomp
       if input == "1"
-        self.list_by_name
-      elsif input == "2"
         self.list_by_number
-      elsif input == "3"
+      elsif input == "2"
         self.search_name
-      elsif input == "4"
+      elsif input == "3"
         self.search_number
       elsif input.downcase == "exit"
         puts "Thanks for using Pokemon Info, Cya later!"
@@ -24,15 +22,9 @@ class PokemonInfo::CLI
   def display_menu
     puts "Please choose one of the following options or type 'exit' to quit."
     puts "---------------------------------------------------------------------"
-    puts "1. List Pokemon by name"
-    puts "2. List Pokemon by number"
-    puts "3. Search by name"
-    puts "4. Search by number"
-    puts "---------------------------------------------------------------------"
-  end
-
-  def list_by_name
-    puts "Here's a list of all the Pokemon in Alphabetical order!"
+    puts "1. List Pokemon by number"
+    puts "2. Search by name"
+    puts "3. Search by number"
     puts "---------------------------------------------------------------------"
   end
 
@@ -43,6 +35,12 @@ class PokemonInfo::CLI
 
   def search_name
     puts "Enter the name of the Pokemon to display info for"
+    input = gets.chomp
+    puts "Here's the info!"
+  end
+
+  def search_number
+    puts "Enter the number of the Pokemon to display info for"
     input = gets.chomp
     puts "Here's the info!"
   end
