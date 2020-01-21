23cc99af98dd8c3ddce5429df1498f2877da582b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-21 21:57:41 +0000

    Refactored code, added Scraper class.

--- lib/pokemon_info/pokemon.rb
+++ lib/pokemon_info/pokemon.rb
@@ -19,7 +19,7 @@ class PokemonInfo::Pokemon
   end
 
   def self.create_from_pokemondb
-    PokemonInfo::Pokemon.scrape_from_pokemondb
+    PokemonInfo::Scraper.scrape_from_pokemondb
     # PokemonInfo::Pokemon.add_details_to_all_pokemon  decided against loading on launch
   end
 
@@ -27,69 +27,6 @@ class PokemonInfo::Pokemon
     self.all.find {|i| i.name.downcase == pokemon.downcase}
     #=> returns pokemon obj with name of pokemon
   end
-
-  def self.scrape_from_pokemondb
-    # uses nokogiri and open-uri to pull
-    page = Nokogiri::HTML(open("https://pokemondb.net/pokedex/national"))
-    pokemon = page.css("div.infocard-list.infocard-list-pkmn-lg div.infocard")
-    pokemon.each do |mon|
-      info = mon.css("span.infocard-lg-data.text-muted")
-      name = info.css("a")[0].text #holds pokemon name
-      types = []
-      if info.css("a").size ==  3
-        types = [info.css("a")[1].text, info.css("a")[2].text] # [1] and [2] hold the types
-      else
-        types = [info.css("a")[1].text]
-      end
-      PokemonInfo::Pokemon.new(name, types)
-    end
-  end
-
-  def self.add_details_to(pokemon)
-    if pokemon.number < 810
-      specs_page = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/#{pokemon.info_link_name.downcase}"))
-      # adding abilities to pokemon
-      abilities = specs_page.css("div.pokemon-ability-info.color-bg.color-lightblue.match.active div.pokemon-ability-info-detail.match")
-      abilities_and_info = []
-      abilities.each do |ability|
-        abilities_and_info << ability.css("h3").text #the name of the ability
-        abilities_and_info << ability.css("p").text #description of ability
-      end
-      pokemon.abilities = abilities_and_info
-      # adding weaknesses to pokemon
-      weaknesses_raw = specs_page.css("div.pokedex-pokemon-attributes.active div.dtm-weaknesses ul li")
-      weaknesses = []
-      weaknesses_raw.each do |weakness|
-        weaknesses << weakness.css("a span").text.gsub(/\s+/, "")
-      end
-      pokemon.weaknesses = weaknesses
-    end
-  end
-
-  # def self.add_details_to_all_pokemon
-  #   # uses info link and returns extra info to make Pokemon
-  #   @@all.each do |pokemon|
-  #     if pokemon.number < 810
-  #       puts "Pokemon: #{pokemon.name}"
-  #       specs_page = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/#{pokemon.info_link_name.downcase}"))
-  #       # adding abilities to pokemon
-  #       abilities = specs_page.css("div.pokemon-ability-info.color-bg.color-lightblue.match.active div.pokemon-ability-info-detail.match")
-  #       abilities_and_info = []
-  #       abilities.each do |ability|
-  #         abilities_and_info << ability.css("h3").text #the name of the ability
-  #         abilities_and_info << ability.css("p").text #description of ability
-  #       end
-  #       pokemon.abilities = abilities_and_info
-  #       # adding weaknesses to pokemon
-  #       weaknesses_raw = specs_page.css("div.pokedex-pokemon-attributes.active div.dtm-weaknesses ul li")
-  #       weaknesses = []
-  #       weaknesses_raw.each do |weakness|
-  #         weaknesses << weakness.css("a span").text.gsub(/\s+/, "")
-  #       end
-  #       pokemon.weaknesses = weaknesses
-  #     end
-  #   end
-  # end
 
   def check_edge_cases(name)
     if @number == 29
---
+++ lib/pokemon_info/scraper.rb
@@ -0,0 +1,39 @@
+class PokemonInfo::Scraper
+  def self.scrape_from_pokemondb
+    # uses nokogiri and open-uri to pull
+    page = Nokogiri::HTML(open("https://pokemondb.net/pokedex/national"))
+    pokemon = page.css("div.infocard-list.infocard-list-pkmn-lg div.infocard")
+    pokemon.each do |mon|
+      info = mon.css("span.infocard-lg-data.text-muted")
+      name = info.css("a")[0].text #holds pokemon name
+      types = []
+      if info.css("a").size ==  3
+        types = [info.css("a")[1].text, info.css("a")[2].text] # [1] and [2] hold the types
+      else
+        types = [info.css("a")[1].text]
+      end
+      PokemonInfo::Pokemon.new(name, types)
+    end
+  end
+
+  def self.add_details_to(pokemon)
+    if pokemon.number < 810
+      specs_page = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/#{pokemon.info_link_name.downcase}"))
+      # adding abilities to pokemon
+      abilities = specs_page.css("div.pokemon-ability-info.color-bg.color-lightblue.match.active div.pokemon-ability-info-detail.match")
+      abilities_and_info = []
+      abilities.each do |ability|
+        abilities_and_info << ability.css("h3").text #the name of the ability
+        abilities_and_info << ability.css("p").text #description of ability
+      end
+      pokemon.abilities = abilities_and_info
+      # adding weaknesses to pokemon
+      weaknesses_raw = specs_page.css("div.pokedex-pokemon-attributes.active div.dtm-weaknesses ul li")
+      weaknesses = []
+      weaknesses_raw.each do |weakness|
+        weaknesses << weakness.css("a span").text.gsub(/\s+/, "")
+      end
+      pokemon.weaknesses = weaknesses
+    end
+  end
+end
